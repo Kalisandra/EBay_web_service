ecf0fc6d2cae807f7e11e325bc58a9884d486ce0
Aleksandra Zadorkina <zadorkina_alex@mail.ru>
2020-04-18 11:22:35 +0300

    внесены изменения в модель Favorite_searches

--- webapp/favorite_searches/models.py
+++ webapp/favorite_searches/models.py
@@ -1,16 +1,23 @@
+from datetime import datetime
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 from webapp.model import db
 
 
 class Favorite_searches(db.Model):
     query_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
     query_name = db.Column(db.String(50), nullable=False)
     user_query = db.Column(db.String(100), nullable=False)
     chosen_categoryid = db.Column(db.Integer, nullable=False)
     filter_request = db.Column(db.String(200), nullable=False)
-    date_of_creation = db.Column(db.DateTime, nullable=False)
+    date_of_creation = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    user = relationship('User', backref='favorite_searches')
 
 
     def __repr__(self):
